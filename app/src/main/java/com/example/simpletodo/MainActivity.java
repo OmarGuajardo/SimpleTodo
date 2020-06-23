c5d57ab7a837fe06d1f7911ddaf7f65b5bcc3fa1
Omar Guajardo <omarguajardo@fb.com>
2020-06-23 15:56:01 -0700

    Add Button can now add to the list and it's reflected on the ListView

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -36,9 +37,23 @@ public class MainActivity extends AppCompatActivity {
         items.add("Got to the gym");
         items.add("Get Kids from School");
 
-        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        final ItemsAdapter itemsAdapter = new ItemsAdapter(items);
         rvItems.setAdapter(itemsAdapter);
         rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newToDo = etItem.getText().toString();
+
+                //Add Item to Model
+                items.add(newToDo);
+
+                //Notify the adapter that an item has been inserted
+                itemsAdapter.notifyItemInserted(items.size() - 1);
+                etItem.setText("");
+            }
+        });
     }
 
     //place to define methods
